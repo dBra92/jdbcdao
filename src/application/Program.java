e25b9e0d52339226e5bfb886428acc93f379be02
Danilo Diego Menz Alc <dan92br@gmail.com>
2023-03-31 19:38:43 -0300

    278, '79, 80

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,20 @@
 package application;
 
+import java.util.Date;
+
 import entities.Department;
+import entities.Seller;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		Department dp = new Department(1, "Books");
-		System.out.println(dp);
+		Seller sell = new Seller(1, "Jack", "jack@ggmail.com", new Date(29 / 1 / 1999), 4000.0, dp);
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		System.out.println(sell);
 	}
 
 }
